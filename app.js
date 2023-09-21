381a9657b498ad9d356c56ea6eb5ec0f24cebaf6
Oleg Klyuev <olegkluev1507@gmail.com>
2023-09-21 23:58:58 +0500

    feat: Спринт 14- эндпоинты 2

--- app.js
+++ app.js
@@ -28,14 +28,14 @@ app.use(bodyParser.json());
 app.post(
   '/signup',
   celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().required().min(2).max(30),
-      about: Joi.string().required().min(2).max(30),
-      avatar: Joi.string().regex(/^(https?:\/\/)?([a-z0-9-]+\.)*[a-z0-9-]+\.[a-z]{2,}\/?([^\s]*)$/),
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
-    }),
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(/^(https?:\/\/)?([a-z0-9-]+\.)*[a-z0-9-]+\.[a-z]{2,}\/?([^\s]*)$/),
   }),
+}),
   createUser,
 );
 app.post(
